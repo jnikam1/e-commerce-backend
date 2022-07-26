cc9229bca9c4f5857699090d9d297fc37ffad3fa
Jayesh Nikam <jnikam1@binghamton.edu>
2022-07-25 23:20:40 -0400

    heroku updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const router = require('./routes/e-commerce.routes')
 
@@ -15,14 +16,15 @@ app.get('/', (req, res) => {
 
 app.use('/e-commerce', router)
 
-const PORT = 6000
-app.listen(PORT, () => {
+// const PORT = 6000
+app.listen(process.env.PORT, () => {
     console.log("Listening on 6000")
 
 })
 
-const MONGOURI = "mongodb+srv://jnikam1:1234@cluster0.8uptl.mongodb.net/products?retryWrites=true&w=majority"
-mongoose.connect(MONGOURI)
+// const MONGOURI = "mongodb+srv://jnikam1:1234@cluster0.8uptl.mongodb.net/products?retryWrites=true&w=majority"
+
+mongoose.connect(process.env.MONGOURI)
     .then(() => {
         console.log("Connected to database")
     })
